a0f9554a353a7167d2418557b114dac96c4ad905
Ibdul <ibdul1999@gmail.com>
2021-10-19 12:53:52 +0100

    FRONTEND 3.1
    * manage page linked with backend
    
    bug fix:
    * Button (on axios request) busy indicator fixed
    
    Signed-off-by: Ibdul <ibdul1999@gmail.com>

--- frontend/src/views/events/Manage.vue
+++ frontend/src/views/events/Manage.vue
@@ -69,7 +69,7 @@
 							  :key="ticket.code"
 							  @click="openModal(ticket, 'ticket')"
 							  >
-								<td>{{id}}</td>
+								<td>{{id+1}}</td>
 								<td>{{ticket.ticket_class}}</td>
 								<td>{{ticket.email}}</td>
 								<td>{{ticket.code}}</td>
@@ -83,7 +83,7 @@
 		<footer class="footer">
 
 			<p v-if="step == totalSteps">
-				{{data.tickets.length}} ticket<template v-if="data.tickets.length>1">s</template> registered.
+				<i>{{data.tickets.length}} ticket<template v-if="data.tickets.length>1">s</template> registered.</i>
 			</p>
 			<btn
 				class="btn-primary-clear"
@@ -92,7 +92,7 @@
 				:disabled="buttons.footer.primary.disabled"
 				:class="buttons.footer.primary.status==CONSTANTSX.busy ? 'btn-loading' : '' "
 				>
-                    {{buttons.footer.primary.text}}
+                    {{buttons.footer.primary.status || buttons.footer.primary.text}}
             </btn>
             <btn
 				class="btn-primary-clear"
@@ -161,6 +161,7 @@
 </template>
 
 <script lang="ts">
+	import axios from 'axios'
 
 	import CONSTANTS from '@/consumables/constants'
 	import { useToasts } from '@/consumables/plugins'
@@ -221,89 +222,13 @@
 				"pass": "",
 			})
 			let data= reactive<any>({})
-			const fakeData: any = {
-				event: {
-					account: null,
-					code: "lyvr3ind",
-					created_by: "ibdul1999@gmail.com",
-					description: "farmers hangout",
-					end_date: null,
-					kind: "party",
-					max_guests: null,
-					refund_deadline: null,
-					refundable: false,
-					start_date: "2021-10-17T00:14:00Z",
-					title: "Cultiv8",
-					venue: "Zero's farm yard",
-				},
-				requested_information: [
-					{
-						description: "We need this info in order to keep our records clean",
-						event: "lyvr3ind",
-						id: 1,
-						kind: "text",
-						maxlength: 123,
-						required: false,
-						short_description: "please tell us your nickname",
-						title: "nickname",
-					}
-				],
-				ticket_classes: [
-					{
-						cost: "0.00",
-						description: "Generic class",
-						event: "lyvr3ind",
-						id: 1,
-						title: "regular"
-					},
-					{
-						cost: "123.00",
-						description: "asdf",
-						event: "lyvr3ind",
-						id: 2,
-						title: "Shark",
-					},
-				],
-				tickets: [
-					{
-						code: "ecs8hfqb8",
-						email: "ibdul1999@gmail.com",
-						event: "lyvr3ind",
-						requested_information_answers: [
-							{
-								requested_information: "nickname",
-								answer: "ibdul"
-							}
-						],
-						ticket_class: "regular",
-					},
-					{
-						code: "91g9luh72",
-						email: "someRandomemail@gmail.com",
-						event: "lyvr3ind",
-						requested_information_answers: [],
-						ticket_class: "regular",
-					}
-				]
-			}
+			
 
 			/////// METHODS
 
-			const fakeRequest = (fakeAddress: string, data:any) => {
-				let milliseconds = 200
-				const date = Date.now()
-				let currentDate = null
-				do {
-					currentDate = Date.now()
-				}
-				while (currentDate - date < milliseconds)
-				return true
-			}
-
 			const validate =async(fromNext=false)=>{
 
-				buttons.footer.primary.status = CONSTANTS.busy
-				buttons.footer.primary.disabled = true
+				
 
 				let encounteredError = false
 				if(fromNext){
@@ -311,23 +236,33 @@
 					submitBtn.click()
 				}
 				else{
-					if(step.value == 1){
+					buttons.footer.primary.status = CONSTANTS.busy
+					buttons.footer.primary.disabled = true
 
-						let fakeResponse = fakeRequest(`/api/v1/events/manage/`, payload)
-							if (fakeResponse){
+					if(step.value == 1){
+						await axios
+							.post(`/api/v1/events/manage/`, 
+								payload)
+							.then( (response: ResponseObjectType) => {
 								$toasts.SuccessToast("Event matched")
-								Object.assign(data, fakeData)
-							}
-							else{
-								$toasts.ErrorToast("Something went wrong")
-							}
+								Object.assign(data, response.data)
+							})
+							.catch((error: ResponseErrorObjectType) => {
+								encounteredError = true
+								if (error.response.status==404){
+									$toasts.ErrorToast("Event not found")
+								}
+								else{
+									$toasts.ErrorToast("Something went wrong")
+								}
+							})
 					}
 					if(encounteredError==false){
 						next()
 					}
+				buttons.footer.primary.status = ""
+				buttons.footer.primary.disabled = false
 				}
-					buttons.footer.primary.status = ""
-					buttons.footer.primary.disabled = false
 			}
 			const next = ()=>{
 				if (step.value < totalSteps.value){
